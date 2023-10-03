46ed626b3c601fcc719447fc314378486e2965fa
jullis2003 <lisunjulia@gmail.com>
2023-10-03 19:24:46 +0300

    new changes

--- src/components/header/contacts/Data.vue
+++ src/components/header/contacts/Data.vue
@@ -4,13 +4,13 @@
   >
     <div class="flex items-center lg:space-x-6 md:space-x-2 sm:space-x-1">
       <div class="h-10 w-[1px] bg-[#19191c] md:block sm:hidden"></div>
-      <img src="src\assets\icons\clock.png" class="lg:w-4 sm:w-3" />
+      <img src="\src\assets\icons\clock.png" class="lg:w-4 sm:w-3" />
       <p class="lg:text-xl sm:text-sm">Графік роботи з 11:00 до 2:00</p>
     </div>
     <div class="mobile-line md:hidden sm:block"></div>
     <div class="flex items-center lg:space-x-6 md:space-x-2 sm:space-x-1">
       <div class="h-10 w-[1px] bg-[#19191c] md:block sm:hidden"></div>
-      <img src="src\assets\icons\telephone.png" class="lg:w-4 sm:w-3" />
+      <img src="\src\assets\icons\telephone.png" class="lg:w-4 sm:w-3" />
       <a href="tel:(123) 456 - 789" class="lg:text-xl sm:text-sm"
         >(123) 456 - 789</a
       >
