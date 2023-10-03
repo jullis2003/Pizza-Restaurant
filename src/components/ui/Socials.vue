562690416d33e6a61a7a57809ebb01b15c5528b4
jullis2003 <lisunjulia@gmail.com>
2023-10-03 19:24:02 +0300

    new changes

--- src/components/ui/Socials.vue
+++ src/components/ui/Socials.vue
@@ -2,21 +2,21 @@
   <div class="flex items-center md:space-x-7 sm:space-x-4">
     <a href="">
       <img
-        src="src\assets\icons\facebook.png"
+        src="\src\assets\icons\facebook.png"
         alt="Facebook"
         class="lg:w-8 sm:w-4"
       />
     </a>
     <a href="">
       <img
-        src="src\assets\icons\instagram.png"
+        src="\src\assets\icons\instagram.png"
         alt="Instagram"
         class="lg:w-8 sm:w-4"
       />
     </a>
     <a href="">
       <img
-        src="src\assets\icons\twitter.png"
+        src="\src\assets\icons\twitter.png"
         alt="Twitter"
         class="lg:w-8 sm:w-4"
       />
